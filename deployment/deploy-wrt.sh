0f06b12d41b6edb9cec463008eb220f38c59d429
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2023-06-15 17:49:40 +0500

    Added OpenWRT startup script

---
+++ deployment/deploy-wrt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Updating the system"
+opkg update
+echo "Installing libraries"
+opkg install nano
+opkg install rsync
+opkg install python3
+opkg install python3-pip
+opkg install python3-netifaces
+sudo pip3 install pycryptodome
+echo "Preparing directories"
+mkdir /opt/hip-vpls/
+cd ..
+echo "Copying the files"
+rsync -rv  hiplib  switchd.py  switchfabric.py /opt/hip-vpls/
+echo "Copying the service file"
+cd startup
+cp hip-vpls /etc/init.d/
+/etc/init.d/hip-vpls enable
+/etc/init.d/hip-vpls start
+
