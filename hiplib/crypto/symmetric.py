10db815e58144d8913e52810aed2ec8344b1dd4d
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2023-12-31 14:58:00 +0500

    More improvements to AES HW

--- hiplib/crypto/symmetric.py
+++ hiplib/crypto/symmetric.py
@@ -17,6 +17,24 @@
 
 from Crypto.Cipher import AES
 
+import ctypes
+from ctypes import cdll
+from array import array
+
+
+lib = cdll.LoadLibrary('/opt/hip-vpls/hiplib/crypto/aeslib.so')
+
+class Aes(ctypes.Structure):
+    pass
+
+lib.freeme.argtypes = ctypes.POINTER(ctypes.c_ubyte),
+lib.AES256.argtypes = ctypes.POINTER(ctypes.c_ubyte), 
+lib.AES256.restype = ctypes.c_void_p #ctypes.POINTER(Aes)
+lib.AES256EncryptBlock.argtypes = ctypes.c_void_p, ctypes.c_int, ctypes.POINTER(ctypes.c_ubyte), ctypes.POINTER(ctypes.c_ubyte)
+lib.AES256EncryptBlock.restype = ctypes.POINTER(ctypes.c_ubyte)
+lib.AES256DecryptBlock.argtypes = ctypes.c_void_p, ctypes.c_int, ctypes.POINTER(ctypes.c_ubyte), ctypes.POINTER(ctypes.c_ubyte)
+lib.AES256DecryptBlock.restype = ctypes.POINTER(ctypes.c_ubyte)
+
 class SymmetricCrypto():
 	BLOCK_SIZE = 0x0;
 	KEY_SIZE_BITS = 0x0;
@@ -99,6 +117,27 @@ class AES128CBCCipher(SymmetricCrypto):
 		cipher = AES.new(key, AES128CBCCipher.MODE_CBC, iv);
 		return self.unpad(cipher.decrypt(data));
 
+"""
+class AES256CBCCipher(SymmetricCrypto):
+
+	KEY_SIZE_BITS = 0x20;
+	MODE_CBC = AES.MODE_CBC;
+	BLOCK_SIZE = AES.block_size;
+	ALG_ID = 0x4;
+	
+	def __init__(self):
+		pass
+
+	def encrypt(self, key, iv, data):
+		cipher = AES.new(key, AES256CBCCipher.MODE_CBC, iv);
+		return cipher.encrypt(self.pad(data, AES256CBCCipher.BLOCK_SIZE));
+
+	def decrypt(self, key, iv, data):
+		cipher = AES.new(key, AES256CBCCipher.MODE_CBC, iv);
+		return self.unpad(cipher.decrypt(data));
+"""
+
+"""Hardware accelerate AES256 encryption routines"""
 class AES256CBCCipher(SymmetricCrypto):
 
 	KEY_SIZE_BITS = 0x20;
@@ -116,12 +155,59 @@ class AES256CBCCipher(SymmetricCrypto):
 		"""
 		Encryptes the plaintext using
 		"""
-		cipher = AES.new(key, AES256CBCCipher.MODE_CBC, iv);
-		return cipher.encrypt(self.pad(data, AES256CBCCipher.BLOCK_SIZE));
+		if len(data) % 16 != 0:
+			return
+		#cipher = AES.new(key, AES256CBCCipher.MODE_CBC, iv);
+		#return cipher.encrypt(self.pad(data, AES256CBCCipher.BLOCK_SIZE));
+		#v = array('I', key);
+		#addr, count = v.buffer_info();
+		#pkey = ctypes.cast(addr, ctypes.POINTER(ctypes.c_ubyte))
+		#pkey = (ctypes.c_ubyte * len(key))(*key)
+		v = array('B',key);pkey = (ctypes.c_ubyte * len(v)).from_buffer(v)
+		obj = lib.AES256(pkey)
+
+		#v = array('I', data);
+		#addr, count = v.buffer_info();
+		#pdata = ctypes.cast(addr, ctypes.POINTER(ctypes.c_ubyte))
+		#v = array('I', iv);
+		#addr, count = v.buffer_info();
+		#piv = ctypes.cast(addr, ctypes.POINTER(ctypes.c_ubyte))
+		#pdata = (ctypes.c_ubyte * len(data))(*data)
+		#piv = (ctypes.c_ubyte * len(iv))(*iv)
+		v = array('B',data);pdata = (ctypes.c_ubyte * len(v)).from_buffer(v)
+		v = array('B',iv);piv = (ctypes.c_ubyte * len(v)).from_buffer(v)
+		addr = lib.AES256EncryptBlock(obj, len(data), pdata, piv)
+
+		ciphertext = ctypes.string_at(addr, len(data))
+		lib.freeme(addr);
+		return ciphertext
 
 	def decrypt(self, key, iv, data):
 		"""
 		This method decryptes the ciphertext
 		"""
-		cipher = AES.new(key, AES256CBCCipher.MODE_CBC, iv);
-		return self.unpad(cipher.decrypt(data));
+		if len(data) % 16 != 0:
+			return
+		#cipher = AES.new(key, AES256CBCCipher.MODE_CBC, iv);
+		#return self.unpad(cipher.decrypt(data));
+		#v = array('I', key);
+		#addr, count = v.buffer_info();
+		#pkey = ctypes.cast(addr, ctypes.POINTER(ctypes.c_ubyte))
+		v = array('B',key);pkey = (ctypes.c_ubyte * len(v)).from_buffer(v)
+		#pkey = (ctypes.c_ubyte * len(key))(*key)
+		obj = lib.AES256(pkey)
+
+		#v = array('I', data);
+		#addr, count = v.buffer_info();
+		#pdata = ctypes.cast(addr, ctypes.POINTER(ctypes.c_ubyte))
+		#v = array('I', iv);
+		#addr, count = v.buffer_info();
+		#piv = ctypes.cast(addr, ctypes.POINTER(ctypes.c_ubyte))
+		#pdata = (ctypes.c_ubyte * len(data))(*data)
+		#piv = (ctypes.c_ubyte * len(iv))(*iv)
+		v = array('B',data);pdata = (ctypes.c_ubyte * len(v)).from_buffer(v)
+		v = array('B',iv);piv = (ctypes.c_ubyte * len(v)).from_buffer(v)
+		addr = lib.AES256DecryptBlock(obj, len(data), pdata, piv)
+		plaintext = ctypes.string_at(addr, len(data))
+		lib.freeme(addr);
+		return plaintext
